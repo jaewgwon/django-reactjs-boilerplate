9fe9ecaad78bd9b667f7f803a62554658bd0de1b
Jaewon Gwon <jaewgwon@gmail.com>
2019-06-27 11:06:59 +0900

    First commit
    django + react.js boilerplate.
    I think, using django as an api server seems better.

---
+++ devlog-be/devlog/assets/bundles/precache-manifest.8eb078ced910327b7e3ae2ab89da7875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e1b0a908b0e5cf605a0459e5949d1b",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "97b34b56509fbe656313",
+    "url": "http://localhost:3000/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6c20aeb11eb347977b10",
+    "url": "http://localhost:3000/static/js/2.cea49a55.chunk.js"
+  },
+  {
+    "revision": "97b34b56509fbe656313",
+    "url": "http://localhost:3000/static/js/main.d486495b.chunk.js"
+  },
+  {
+    "revision": "e8d955eac6f650306bb5",
+    "url": "http://localhost:3000/static/js/runtime~main.8a6cbfe7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "http://localhost:3000/static/media/logo.5d5d9eef.svg"
+  }
+]);
